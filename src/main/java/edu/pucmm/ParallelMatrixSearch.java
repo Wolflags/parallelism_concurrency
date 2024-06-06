9b928b87e0019ae7cade13f6ebb73e40895ce6d0
Jose Jaquez <joserafaelrey360@gmail.com>
2024-06-06 09:57:24 -0400

    Examen

--- src/main/java/edu/pucmm/ParallelMatrixSearch.java
+++ src/main/java/edu/pucmm/ParallelMatrixSearch.java
@@ -10,27 +10,81 @@ public class ParallelMatrixSearch {
     private static final int THREAD_COUNT = 4;
     private static final int[][] matrix = new int[MATRIX_SIZE][MATRIX_SIZE];
     private static final int TARGET = 256; // Número a buscar
+    private static long startTime;
+    private static long endTime;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Inicializar la matriz con valores aleatorios
-        //...
+        for(int i=0,j=0; i<MATRIX_SIZE; i++) {
+            for(j=0; j<MATRIX_SIZE; j++) {
+                matrix[i][j] = (int)(Math.random() * 1000);
+            }
+        }
 
         // Medir el tiempo de ejecución de la búsqueda secuencial
-        //...
-        // System.out.println("Resultado búsqueda secuencial: " + ...);
-        // System.out.println("Tiempo búsqueda secuencial: " + (endTime - startTime) + "ms");
+        startTime = System.currentTimeMillis();
+        sequentialSearch();
+        endTime = System.currentTimeMillis();
+
+
+        //System.out.println("Resultado búsqueda secuencial: " + ...);
+        System.out.println("Tiempo búsqueda secuencial: " + (endTime - startTime) + "ms");
 
         // Medir el tiempo de ejecución de la búsqueda paralela
         //...
+        startTime = System.currentTimeMillis();
+        parallelSearch();
+        endTime = System.currentTimeMillis();
         // System.out.println("Resultado búsqueda paralela: " + ...);
-        // System.out.println("Tiempo búsqueda paralela: " + (endTime - startTime) + "ms");
+        System.out.println("Tiempo búsqueda paralela: " + (endTime - startTime) + "ms");
     }
 
     private static void sequentialSearch() {
-        //...
+        for(int i=0; i<MATRIX_SIZE; i++) {
+            for(int j=0; j<MATRIX_SIZE; j++) {
+                if(matrix[i][j] == TARGET) {
+                    System.out.println("Elemento encontrado en la posición [" + i + "][" + j + "]");
+                    return;
+                }
+            }
+        }
     }
 
-    private static void parallelSearch() {
-        //...
+    private static void parallelSearch() throws InterruptedException {
+        Thread[] threads = new Thread[THREAD_COUNT];
+        for(int i=0;i<THREAD_COUNT;i++){
+            int Inicio = i*(matrix.length/THREAD_COUNT);
+            int Final = (i+1)*(matrix.length/THREAD_COUNT);
+            threads[i] = new Thread(new Busqueda(Inicio,Final));
+        }
+        for(int i=0;i<THREAD_COUNT;i++){
+            threads[i].start();
+            threads[i].join();
+        }
+
     }
+
+    public static class Busqueda implements Runnable{
+        private int Inicio;
+        private int Final;
+
+        public Busqueda(int Inicio, int Final){
+            this.Inicio = Inicio;
+            this.Final = Final;
+        }
+
+        @Override
+        public void run(){
+            for(int i=Inicio; i<Final; i++) {
+                for(int j=0; j<MATRIX_SIZE; j++) {
+                    if(matrix[i][j] == TARGET) {
+                        System.out.println("Elemento encontrado en la posición [" + i + "][" + j + "]");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+
 }
--- src/main/java/edu/pucmm/ProducerConsumer.java
+++ src/main/java/edu/pucmm/ProducerConsumer.java
@@ -1,5 +1,6 @@
 package edu.pucmm;
 
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -12,25 +13,69 @@ public class ProducerConsumer {
     private static final int PRODUCER_COUNT = 2;
     private static final int CONSUMER_COUNT = 2;
     private static final int PRODUCE_COUNT = 100;
+    private static int PRODUCIDO = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+        Thread[] producers = new Thread[PRODUCER_COUNT];
+        Thread[] consumers = new Thread[CONSUMER_COUNT];
+        for (int i = 0; i < PRODUCER_COUNT; i++) {
+            producers[i] = new Thread(new Producer(queue));
+        }
+        for (int i = 0; i < CONSUMER_COUNT; i++) {
+            consumers[i] = new Thread(new Consumer(queue));
+        }
+        for (int i = 0; i < PRODUCER_COUNT; i++) {
+        producers[i].start();
+        }
+        for (int i = 0; i < CONSUMER_COUNT; i++) {
+            consumers[i].start();
+        }
+
 
     }
 
     static class Producer implements Runnable {
+        private BlockingQueue<Integer> queue;
+
+        public Producer(BlockingQueue<Integer> queue) {
+            this.queue = queue;
+        }
 
         @Override
         public void run() {
+            while (PRODUCIDO < PRODUCE_COUNT) {
+                int Random = new Random().nextInt();
+                while (queue.remainingCapacity() == 0) {
+                    Thread.yield();
+                }
+                queue.add(Random);
+                System.out.println("Producido " + Random);
+                PRODUCIDO++;
+            }
+
+
 
         }
     }
 
     static class Consumer implements Runnable {
 
+        private BlockingQueue<Integer> queue;
+
+        public Consumer(BlockingQueue<Integer> queue) {
+            this.queue = queue;
+        }
+
         @Override
         public void run() {
-
+            try {
+                for(int i=0;i<PRODUCE_COUNT;i++){
+                    System.out.println("Consumido " + queue.take());
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
